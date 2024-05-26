aadfa90d455f9d8ce668eacc6934040939043296
Maya Warrier <maya.warrierm@gmail.com>
2024-05-26 02:56:49 -0400

    Finally got SDL2 dll working with CMake - this was far more complicated than it needed to be - might need to use the FindSDL2 thing later on for other platforms, for now this works on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,46 @@
 
+
+
 #include "i8080.hpp"
 #include "i8080_opcodes.h"
 
 
-
+#include <stdio.h>
+#include <SDL.h>
 
 int main(int argc, char** argv)
 {
-    
+    SDL_Window* window;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    //Create window
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    //Get window surface
+    screenSurface = SDL_GetWindowSurface(window);
+
+    //Fill the surface white
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+    //Update the surface
+    SDL_UpdateWindowSurface(window);
+
+    //Hack to get window to stay up
+    SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+
+    return 0;
 }
